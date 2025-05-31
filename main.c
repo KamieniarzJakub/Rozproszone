a589882be648ff3a5b2d71a855901eb38249b737
Tomasz Pawłowski <t.pawlowski03@gmail.com>
2025-05-31 18:28:55 +0200

    some fixes

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
+#include <assert.h>
 #include <mpi.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <signal.h>
 
 pthread_t receiver_thread;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-bool can_proceed = false;
 
 // Rodzaje wiadomości
 enum MESSAGES {
@@ -19,31 +19,10 @@ enum MESSAGES {
   TAG_REQ = 1,
   // Potwierdzenie i zezwolenie na zabranie zasobu przez inny proces
   TAG_ACK = 2,
-  TAG_REL = 3,   // Opuszczenie sekcji krytycznej, useless?
+  TAG_REL = 3,   // Opuszczenie sekcji krytycznej
   TAG_EMPTY = 5, // Nowy pusty słoik
   TAG_FULL = 6   // Nowa konfitura
 };
-
-const int ROOT = 0; // Proces główny
-
-int P = 0; // maksymalna liczba sloikow
-int K = 0; // maksymalna liczba konfitur
-int B = 3; // liczba babć
-int S = 4; // liczba studentek
-bool csv_mode = false;
-#define MAX_PROCESSES 32
-
-int clockLamport = 0;
-int rank, size;
-bool waiting_ack[MAX_PROCESSES];
-int ack_count = 0;
-bool has_jar = false;
-bool has_jam = false;
-bool is_babcia = false;
-bool is_studentka = false;
-int liczba_sloikow = 0;
-int liczba_konfitur = 0;
-
 
 typedef struct {
   int ts;   // zegar Lamporta
@@ -53,25 +32,32 @@ typedef struct {
 
 MPI_Datatype MPI_PACKET_T;
 
-// Oddzielne kolejki
-packet_t queue_sloiki[MAX_PROCESSES];
-int queue_sloiki_size = 0;
+int P = 0; // maksymalna liczba sloikow
+int K = 0; // maksymalna liczba konfitur
+int B = 3; // liczba babć
+int S = 4; // liczba studentek
+bool csv_mode = false;
 
-packet_t queue_konfitury[MAX_PROCESSES];
-int queue_konfitury_size = 0;
+int clockLamport = 0;
+int rank, size;
+bool *waiting_ack = NULL; // Tablica o rozmiarze procesów tego samego typu co
+                          // obecny przechowująca odebrane potwierdzenia od
+                          // danego procesu, indeks w tablicy odpowiada tid
+int ack_count = 0;
+
+bool has_jar = false;
+bool has_jam = false;
+bool is_babcia = false;
+bool is_studentka = false;
+int liczba_sloikow = 0;
+int liczba_konfitur = 0;
+
+packet_t *deffered_queue = NULL;
+size_t deferred_queue_size = 0;
 
 void inc_clock(int received_ts) {
   clockLamport = (clockLamport > received_ts ? clockLamport : received_ts) + 1;
 }
-
-bool is_first_in_queue() {
-  if (is_babcia) {
-    return queue_sloiki_size > 0 && queue_sloiki[0].src == rank;
-  } else {
-    return queue_konfitury_size > 0 && queue_konfitury[0].src == rank;
-  }
-}
-
 
 bool receive_condition() {
   bool all_ack_received;
@@ -87,36 +73,37 @@ bool receive_condition() {
   return !(all_ack_received && resources_available);
 }
 
-void print_queue() {
-  printf("[Rank %d][Clock %d] [kolejka sloiki]: ", rank, clockLamport);
-  for (int i = 0; i < queue_sloiki_size; i++) {
-    printf("%d(ts=%d) ", queue_sloiki[i].src, queue_sloiki[i].ts);
-  }
-  printf("| [kolejka konfitury]: ");
-  for (int i = 0; i < queue_konfitury_size; i++) {
-    printf("%d(ts=%d) ", queue_konfitury[i].src, queue_konfitury[i].ts);
-  }
-  printf("\n");
-  fflush(stdout);
-}
+// void print_queues(packet_t queue[], size_t queue_size) {
+//   printf("[Rank %d][Clock %d] [kolejka sloiki]: ", rank, clockLamport);
+//   for (int i = 0; i < queue_sloiki_size; i++) {
+//     printf("%d(ts=%d) ", queue_sloiki[i].src, queue_sloiki[i].ts);
+//   }
+//   printf("| [kolejka konfitury]: ");
+//   for (int i = 0; i < queue_konfitury_size; i++) {
+//     printf("%d(ts=%d) ", queue_konfitury[i].src, queue_konfitury[i].ts);
+//   }
+//   printf("\n");
+//   fflush(stdout);
+// }
 
-void list_to_str(packet_t* queue, int len, char** out_ptr) {
+void list_to_str(packet_t *queue, int len, char **out_ptr) {
   int current_len = 1; // space for the null terminator
-  *out_ptr = (char*)malloc(current_len * sizeof(char));
+  *out_ptr = (char *)malloc(current_len * sizeof(char));
   if (*out_ptr == NULL) {
     fprintf(stderr, "MALLOC ERROR\n");
     exit(1);
   }
   (*out_ptr)[0] = '\0';
 
-  char* out = *out_ptr;
+  char *out = *out_ptr;
   int index = 0;
 
   for (int i = 0; i < len; i++) {
-    int needed = snprintf(NULL, 0, "%d->%d,", (queue + i)->src, (queue + i)->ts);
+    int needed =
+        snprintf(NULL, 0, "%d->%d,", (queue + i)->src, (queue + i)->ts);
     if (index + needed >= current_len) {
       current_len += needed + 1; // +1 for next comma and null terminator
-      char* temp = (char*)realloc(out, current_len * sizeof(char));
+      char *temp = (char *)realloc(out, current_len * sizeof(char));
       if (temp == NULL) {
         fprintf(stderr, "REALLOC ERROR\n");
         free(out);
@@ -134,29 +121,25 @@ void list_to_str(packet_t* queue, int len, char** out_ptr) {
   }
 }
 void debug(const char *message) {
-  const char *role = is_babcia ? "Babcia" : (is_studentka ? "Studentka" : "Proces");
+  const char *role =
+      is_babcia ? "Babcia" : (is_studentka ? "Studentka" : "Proces");
   const int required_ack = is_babcia ? B - 1 : S - 1;
 
-  if (csv_mode){
-    char* out_konfitury = NULL;
-    list_to_str(queue_konfitury,queue_konfitury_size,&out_konfitury);
+  if (csv_mode) {
+    char *out_queue = NULL;
+    list_to_str(deffered_queue, deferred_queue_size, &out_queue);
 
-    char* out_sloiki = NULL;
-    list_to_str(queue_sloiki,queue_sloiki_size,&out_sloiki);
+    printf("%d;%d;%s;\"%s\";%d;%d;%d;%d;\"%s\";%d;%d\n", rank, clockLamport,
+           role, message, liczba_sloikow, liczba_konfitur, has_jar, has_jam,
+           out_queue, ack_count, required_ack);
 
-    printf("%d;%d;%s;\"%s\";%d;%d;%d;%d;\"%s\";\"%s\";%d;%d\n",
-      rank,clockLamport,role,message,liczba_sloikow,liczba_konfitur,
-      has_jar,has_jam,out_sloiki,out_konfitury,ack_count,required_ack
-    );
-
-    free(out_konfitury);
-    free(out_sloiki);
+    free(out_queue);
   } else {
-      printf("[%d][%d][%s] %s [sloiki: %d, konfitury: %d, has_jar: %d, has_jam: "
-        "%d, ACK: %d/%d]\n",
-        rank, clockLamport, role, message, liczba_sloikow, liczba_konfitur,
-        has_jar, has_jam, ack_count, required_ack);
-    print_queue();
+    printf("[%d][%d][%s] %s [sloiki: %d, konfitury: %d, has_jar: %d, has_jam: "
+           "%d, ACK: %d/%d]\n",
+           rank, clockLamport, role, message, liczba_sloikow, liczba_konfitur,
+           has_jar, has_jam, ack_count, required_ack);
+    // print_queues();
     fflush(stdout);
   }
 }
@@ -164,13 +147,6 @@ void debug(const char *message) {
 void send_packet(int dst, int tag) {
   packet_t pkt = {.ts = clockLamport, .src = rank, .type = tag};
   MPI_Send(&pkt, 1, MPI_PACKET_T, dst, tag, MPI_COMM_WORLD);
-}
-
-void broadcast_packet(int tag) {
-  for (int i = 0; i < size; i++) {
-    if (i != rank)
-      send_packet(i, tag);
-  }
 }
 
 int compare_packet(const void *a, const void *b) {
@@ -182,32 +158,21 @@ int compare_packet(const void *a, const void *b) {
 }
 
 void add_to_queue(packet_t pkt) {
-  if (is_babcia || pkt.src < B) {
-    queue_sloiki[queue_sloiki_size++] = pkt;
-    qsort(queue_sloiki, queue_sloiki_size, sizeof(packet_t), compare_packet);
-  } else {
-    queue_konfitury[queue_konfitury_size++] = pkt;
-    qsort(queue_konfitury, queue_konfitury_size, sizeof(packet_t),
-          compare_packet);
-  }
+  deffered_queue[deferred_queue_size++] = pkt;
+  qsort(deffered_queue, deferred_queue_size, sizeof(packet_t), compare_packet);
 }
 
-
 void remove_from_queue(int src) {
-  int size = is_babcia ? queue_sloiki_size : queue_konfitury_size;
-  packet_t *queue = is_babcia ? queue_sloiki : queue_konfitury;
-  for (int i = 0; i < size; i++) {
-    if (queue[i].src == src) {
-      for (int j = i; j < size - 1; j++) {
-        queue[j] = queue[j + 1];
+  for (int i = 0; i < deferred_queue_size; i++) {
+    if (deffered_queue[i].src == src) {
+      for (int j = i; j < deferred_queue_size - 1; j++) {
+        deffered_queue[j] = deffered_queue[j + 1];
       }
       size--;
       break;
     }
   }
 }
-
-
 
 const char *tag_status_disp(int tag) {
   switch (tag) {
@@ -226,59 +191,62 @@ const char *tag_status_disp(int tag) {
   }
 }
 
-
 void *receive_thread_func(void *arg) {
   packet_t pkt;
   MPI_Status status;
 
   while (true) {
-    MPI_Recv(&pkt, 1, MPI_PACKET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+    MPI_Recv(&pkt, 1, MPI_PACKET_T, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD,
+             &status);
 
     pthread_mutex_lock(&mutex);
     inc_clock(pkt.ts);
 
     char buf[128];
-    snprintf(buf, sizeof(buf), "Otrzymałam %s od [%d]", tag_status_disp(status.MPI_TAG), pkt.src);
+    snprintf(buf, sizeof(buf), "Otrzymałam %s od [%d]",
+             tag_status_disp(status.MPI_TAG), pkt.src);
 
     switch (status.MPI_TAG) {
-      case TAG_REQ:
-        if ((is_babcia && pkt.src < B) || (is_studentka && pkt.src >= B && pkt.src < B + S)) {
-          add_to_queue(pkt);
-          // Najpierw trzeba sprawdzić czy nie jesteśmy obecnie w sekcji krytycznej, jeśli tak to zapisujemy to do kolejki
-          // W przeciwnym razie wysyłamy odpowiedź
+    case TAG_REQ:
+      if ((is_babcia && pkt.src < B) ||
+          (is_studentka && pkt.src >= B && pkt.src < B + S)) {
+        add_to_queue(pkt);
+        // Najpierw trzeba sprawdzić czy nie jesteśmy obecnie w sekcji
+        // krytycznej, jeśli tak to zapisujemy to do kolejki W przeciwnym razie
+        // wysyłamy odpowiedź
+        // TODO:
 
-          send_packet(pkt.src, TAG_ACK);
-          debug(buf);
-        }
-        break;
-      case TAG_ACK:
-        if (!waiting_ack[pkt.src]) {
-          ack_count++;
-          waiting_ack[pkt.src] = true;
-          debug(buf);
-        }
-        break;
-      case TAG_REL:
-        remove_from_queue(pkt.src);
-        if (pkt.src < B) {
-          liczba_sloikow--;
-        } else {
-          liczba_konfitur--;
-        }
+        send_packet(pkt.src, TAG_ACK);
         debug(buf);
-        break;
-      case TAG_EMPTY:
-        liczba_sloikow++;
+      }
+      break;
+    case TAG_ACK:
+      if (!waiting_ack[pkt.src]) {
+        ack_count++;
+        waiting_ack[pkt.src] = true;
         debug(buf);
-        break;
-      case TAG_FULL:
-        liczba_konfitur++;
-        debug(buf);
-        break;
+      }
+      break;
+    case TAG_REL:
+      remove_from_queue(pkt.src);
+      if (pkt.src < B) {
+        liczba_sloikow--;
+      } else {
+        liczba_konfitur--;
+      }
+      debug(buf);
+      break;
+    case TAG_EMPTY:
+      liczba_sloikow++;
+      debug(buf);
+      break;
+    case TAG_FULL:
+      liczba_konfitur++;
+      debug(buf);
+      break;
     }
 
     if (!receive_condition()) {
-      can_proceed = true;
       pthread_cond_signal(&cond);
     }
 
@@ -296,21 +264,24 @@ void wait_until_can_proceed() {
   pthread_mutex_unlock(&mutex);
 }
 
-
 void request_resource() {
   pthread_mutex_lock(&mutex);
   clockLamport++;
-  memset(waiting_ack, 0, sizeof(waiting_ack));
+  memset(waiting_ack, 0, (is_babcia ? B : S) * sizeof(bool));
   ack_count = 0;
   packet_t pkt = {.ts = clockLamport, .src = rank, .type = TAG_REQ};
   // add_to_queue(pkt);
 
   if (is_babcia) {
-    for (int i = 0; i < rank; i++) send_packet(i, TAG_REQ);
-    for (int i = rank + 1; i < B; i++) send_packet(i, TAG_REQ);
+    for (int i = 0; i < rank; i++)
+      send_packet(i, TAG_REQ);
+    for (int i = rank + 1; i < B; i++)
+      send_packet(i, TAG_REQ);
   } else if (is_studentka) {
-    for (int i = B; i < rank; i++) send_packet(i, TAG_REQ);
-    for (int i = rank + 1; i < B + S; i++) send_packet(i, TAG_REQ);
+    for (int i = B; i < rank; i++)
+      send_packet(i, TAG_REQ);
+    for (int i = rank + 1; i < B + S; i++)
+      send_packet(i, TAG_REQ);
   }
 
   debug(is_babcia ? "Wysyłam prośbę o słoik" : "Wysyłam prośbę o konfiturę");
@@ -345,9 +316,12 @@ void enter_critical_section() {
     // TODO
   }
 
-
   // TODO: Instead of broadcast to all the process should ONLY send messages to:
   //  - TAG_ACK message to all the deferred requests.
+  for (int i = 0; i < size; i++) {
+    if (i != rank)
+      send_packet(i, TAG_REL);
+  }
 
   // remove_from_queue(rank);
   debug("Wysyłam REL do wszystkich, wychodzę z krytycznej");
@@ -412,7 +386,6 @@ void run_process() {
   }
 }
 
-
 void init_packet_type() {
   const int count = 3;
   int lengths[] = {1, 1, 1};
@@ -423,22 +396,28 @@ void init_packet_type() {
   MPI_Type_commit(&MPI_PACKET_T);
 }
 
-void finalize(int signo){
+void finalize(int signo) {
   MPI_Type_free(&MPI_PACKET_T);
   MPI_Finalize();
+  free(waiting_ack);
+  free(deffered_queue);
 }
 
 int main(int argc, char **argv) {
+  // Inicjalizacja MPI
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  signal(SIGINT,finalize);
-  signal(SIGCHLD,finalize);
-  signal(SIGKILL,finalize);
+  signal(SIGINT, finalize);
+  signal(SIGCHLD, finalize);
+  signal(SIGKILL, finalize);
 
+  // Parsowanie parametrów wejściowych (liczba babć, liczba słoików, czy
+  // zapisywać jako csv)
   if (argc < 2) {
     if (rank == 0)
-      fprintf(stderr, "Użycie: %s <ile_babci> [liczba_sloikow] [csv]\n", argv[0]);
+      fprintf(stderr, "Użycie: %s <ile_babci> [liczba_sloikow] [csv]\n",
+              argv[0]);
     MPI_Abort(MPI_COMM_WORLD, 1);
     return 1;
   }
@@ -466,21 +445,36 @@ int main(int argc, char **argv) {
   S = size - B;
   K = P;
   liczba_sloikow = P;
-  if (argc >= 4 && atoi(argv[3])) csv_mode = true;
+  if (argc >= 4 && atoi(argv[3]))
+    csv_mode = true;
 
   if (csv_mode && rank == 0) {
-    printf("rank;clock;proc_type;message;sloiki;konfitury;has_jar;has_jam;jar_queue;jam_queue;recv_ack;needed_ack\n");
+    printf("rank;clock;proc_type;message;sloiki;konfitury;has_jar;has_jam;jar_"
+           "queue;jam_queue;recv_ack;needed_ack\n");
   }
 
   srand(time(NULL) + rank);
   init_packet_type();
   is_babcia = (rank < B);
   is_studentka = (rank >= B && rank < B + S);
+  if (is_babcia) {
+    waiting_ack = malloc(B * sizeof(bool));
+    deffered_queue = malloc(B * sizeof(bool));
+  } else {
+    waiting_ack = malloc(S * sizeof(bool));
+    deffered_queue = malloc(S * sizeof(bool));
+  }
+  if (waiting_ack == 0) {
+    fprintf(stderr, "MALLOC FAILED");
+    exit(1);
+  }
 
   debug("Start procesu");
 
+  // Stworzenie dodatkowego procesu do komunikacji
   pthread_create(&receiver_thread, NULL, receive_thread_func, NULL);
 
+  // Główny proces przetwarzania
   run_process();
 
   return 0;
